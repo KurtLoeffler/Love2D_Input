b606cdccb89fd32f620641f25125e0b2fc8f9cd4
Kurt Loeffler <kurtloeffler@gmail.com>
2019-09-15 18:32:27 -0700

    cleanup

--- Input.lua
+++ Input.lua
@@ -50,8 +50,6 @@ function Input:keyMouseButtonTriggerTest(trigger, keyFunc, mouseFunc)
 end
 
 function Input:evaluateTriggerTable(triggerTable)
-	local on = triggerTable.on or "pressed"
-	
 	local onPressed = triggerTable.pressed
 	local onDown = triggerTable.down
 	local onReleased = triggerTable.released
@@ -292,7 +290,7 @@ end
 function Input:onUpdate()
 	for _, action in pairs(self.actions) do
 		if self:evaluateFilters(action) then
-			local v1, v2 = self:evaluateTriggers(action, true)
+			local v1, v2 = self:evaluateTriggers(action)
 			if self:evaluateTriggers(action) then
 				self:invokeEvents(action, v1, v2)
 			end
